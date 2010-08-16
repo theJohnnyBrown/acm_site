13270a8c6d5a980b4295b90329364d8bae0f1eb6
Garret <garretjames@gmail.com>
2010-08-16 17:20:44 -0500

    Added role column to people

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100803232712) do
+ActiveRecord::Schema.define(:version => 20100816203503) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20100803232712) do
     t.string   "encrypted_password"
     t.string   "salt"
     t.string   "remember_token"
+    t.string   "role"
   end
 
   add_index "people", ["email"], :name => "index_people_on_email", :unique => true
